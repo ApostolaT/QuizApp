b9cef07b11751a5765a1224bc03e03c9a1a17f77
apostola <alexei.postolachi@evozon.com>
2020-03-16 15:10:53 +0200

    code review changes 2

--- config/MVCConfig.php
+++ config/MVCConfig.php
@@ -13,259 +13,259 @@ $configuration = [
         "routes" => [
             "log_in_page" => [
                 Router::CONFIG_KEY_PATH => "/",
-                "controller" => "login",
-                "action" => "getLoginPage",
-                "method" => "GET",
-                "attributes" => []
+                Router::CONFIG_KEY_CONTROLLER => "login",
+                Router::CONFIG_KEY_ACTION => "getLoginPage",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => []
             ],
             "user_log_in" => [
                 Router::CONFIG_KEY_PATH => "/login",
-                "controller" => "login",
-                "action" => "loginAction",
-                "method" => "POST",
-                "attributes" => []
+                Router::CONFIG_KEY_CONTROLLER => "login",
+                Router::CONFIG_KEY_ACTION => "loginAction",
+                Router::CONFIG_KEY_METHOD => "POST",
+                Router::CONFIG_KEY_ATTRIBUTES => []
             ],
             "user_log_out" => [
                 Router::CONFIG_KEY_PATH => "/logout",
-                "controller" => "login",
-                "action" => "logoutAction",
-                "method" => "GET",
-                "attributes" => []
+                Router::CONFIG_KEY_CONTROLLER => "login",
+                Router::CONFIG_KEY_ACTION => "logoutAction",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => []
             ],
             "list_all_quizzes" => [
                 Router::CONFIG_KEY_PATH => "/quiz/{page}",
-                "controller" => "quiz",
-                "action" => "listAll",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "quiz",
+                Router::CONFIG_KEY_ACTION => "listAll",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "page" => "\d+"
                 ]
             ],
              "view_add_quiz" => [
                 Router::CONFIG_KEY_PATH => "/quiz/add",
-                "controller" => "quiz",
-                "action" => "goToAddQuiz",
-                "method" => "GET",
-                "attributes" => []
+                Router::CONFIG_KEY_CONTROLLER => "quiz",
+                Router::CONFIG_KEY_ACTION => "goToAddQuiz",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => []
             ],
             "add_quiz" => [
                 Router::CONFIG_KEY_PATH => "/quiz/add",
-                "controller" => "quiz",
-                "action" => "addQuiz",
-                "method" => "POST",
-                "attributes" => []
+                Router::CONFIG_KEY_CONTROLLER => "quiz",
+                Router::CONFIG_KEY_ACTION => "addQuiz",
+                Router::CONFIG_KEY_METHOD => "POST",
+                Router::CONFIG_KEY_ATTRIBUTES => []
             ],
             "delete_quiz" => [
                 Router::CONFIG_KEY_PATH => "/quiz/delete/{id}",
-                "controller" => "quiz",
-                "action" => "deleteQuiz",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "quiz",
+                Router::CONFIG_KEY_ACTION => "deleteQuiz",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "id" => '\d+'
                 ]
             ],
             "update_quiz_page" => [
                 Router::CONFIG_KEY_PATH => "/quiz/add/{id}",
-                "controller" => "quiz",
-                "action" => "getUpdateQuizPage",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "quiz",
+                Router::CONFIG_KEY_ACTION => "getUpdateQuizPage",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "id" => '\d+'
                 ]
             ],
             "update_quiz" => [
                 Router::CONFIG_KEY_PATH => "/quiz/add/{id}",
-                "controller" => "quiz",
-                "action" => "updateQuiz",
-                "method" => "POST",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "quiz",
+                Router::CONFIG_KEY_ACTION => "updateQuiz",
+                Router::CONFIG_KEY_METHOD => "POST",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "id" => '\d+'
                 ]
             ],
             "list_all_users" => [
                 Router::CONFIG_KEY_PATH => "/user/{page}",
-                "controller" => "user",
-                "action" => "listAll",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "user",
+                Router::CONFIG_KEY_ACTION => "listAll",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "page" => "\d+"
                 ]
             ],
             "view_add_user" => [
                 Router::CONFIG_KEY_PATH => "/user/add",
-                "controller" => "user",
-                "action" => "goToAddUser",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "user",
+                Router::CONFIG_KEY_ACTION => "goToAddUser",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "page" => "\d+"
                 ]
             ],
             "add_user" => [
                 Router::CONFIG_KEY_PATH => "/user/add",
-                "controller" => "user",
-                "action" => "addUser",
-                "method" => "POST",
-                "attributes" => []
+                Router::CONFIG_KEY_CONTROLLER => "user",
+                Router::CONFIG_KEY_ACTION => "addUser",
+                Router::CONFIG_KEY_METHOD => "POST",
+                Router::CONFIG_KEY_ATTRIBUTES => []
             ],
             "delete_user" => [
                 Router::CONFIG_KEY_PATH => "/user/delete/{id}",
-                "controller" => "user",
-                "action" => "deleteUser",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "user",
+                Router::CONFIG_KEY_ACTION => "deleteUser",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "id" => '\d+'
                 ]
             ],
             "update_user_page" => [
                 Router::CONFIG_KEY_PATH => "/user/add/{id}",
-                "controller" => "user",
-                "action" => "getUpdateUserPage",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "user",
+                Router::CONFIG_KEY_ACTION => "getUpdateUserPage",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "id" => '\d+'
                 ]
             ],
             "update_user" => [
                 Router::CONFIG_KEY_PATH => "/user/add/{id}",
-                "controller" => "user",
-                "action" => "updateUser",
-                "method" => "POST",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "user",
+                Router::CONFIG_KEY_ACTION => "updateUser",
+                Router::CONFIG_KEY_METHOD => "POST",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "id" => '\d+'
                 ]
             ],
             "list_all_questions" => [
                 Router::CONFIG_KEY_PATH => "/question/{page}",
-                "controller" => "question",
-                "action" => "listAll",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "question",
+                Router::CONFIG_KEY_ACTION => "listAll",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "page" => "\d+"
                 ]
             ],
             "view_add_question" => [
                 Router::CONFIG_KEY_PATH => "/question/add",
-                "controller" => "question",
-                "action" => "goToAddQuestion",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "question",
+                Router::CONFIG_KEY_ACTION => "goToAddQuestion",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "page" => "\d+"
                 ]
             ],
             "add_question" => [
                 Router::CONFIG_KEY_PATH => "/question/add",
-                "controller" => "question",
-                "action" => "addQuestion",
-                "method" => "POST",
-                "attributes" => []
+                Router::CONFIG_KEY_CONTROLLER => "question",
+                Router::CONFIG_KEY_ACTION => "addQuestion",
+                Router::CONFIG_KEY_METHOD => "POST",
+                Router::CONFIG_KEY_ATTRIBUTES => []
             ],
             "delete_question" => [
                 Router::CONFIG_KEY_PATH => "/question/delete/{id}",
-                "controller" => "question",
-                "action" => "deleteQuestion",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "question",
+                Router::CONFIG_KEY_ACTION => "deleteQuestion",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "id" => '\d+'
                 ]
             ],
             "update_user_question" => [
                 Router::CONFIG_KEY_PATH => "/question/add/{id}",
-                "controller" => "question",
-                "action" => "getUpdateQuestionPage",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "question",
+                Router::CONFIG_KEY_ACTION => "getUpdateQuestionPage",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "id" => '\d+'
                 ]
             ],
             "update_question" => [
                 Router::CONFIG_KEY_PATH => "/question/add/{id}",
-                "controller" => "question",
-                "action" => "updateQuestion",
-                "method" => "POST",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "question",
+                Router::CONFIG_KEY_ACTION => "updateQuestion",
+                Router::CONFIG_KEY_METHOD => "POST",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "id" => '\d+'
                 ]
             ],
             "get_or_create_quiz_instance" => [
                 Router::CONFIG_KEY_PATH => "/quizInstance/add/{id}",
-                "controller" => "quizInstance",
-                "action" => "createQuizInstance",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "quizInstance",
+                Router::CONFIG_KEY_ACTION => "createQuizInstance",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "id" => '\d+'
                 ]
             ],
             "create_question_instances" => [
                 Router::CONFIG_KEY_PATH => "/questionInstance/add",
-                "controller" => "questionInstance",
-                "action" => "createQuestionInstances",
-                "method" => "GET",
-                "attributes" => []
+                Router::CONFIG_KEY_CONTROLLER => "questionInstance",
+                Router::CONFIG_KEY_ACTION => "createQuestionInstances",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => []
             ],
             "get_question_instance" => [
                 Router::CONFIG_KEY_PATH => "/quiz/question/{offset}",
-                "controller" => "questionInstance",
-                "action" => "getQuestionPage",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "questionInstance",
+                Router::CONFIG_KEY_ACTION => "getQuestionPage",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "offset" => '\d+'
                 ]
             ],
             "set_text_instance_id" => [
                 Router::CONFIG_KEY_PATH => "/quiz/text/add",
-                "controller" => "questionInstance",
-                "action" => "updateTextInstance",
-                "method" => "POST",
-                "attributes" => []
+                Router::CONFIG_KEY_CONTROLLER => "questionInstance",
+                Router::CONFIG_KEY_ACTION => "updateTextInstance",
+                Router::CONFIG_KEY_METHOD => "POST",
+                Router::CONFIG_KEY_ATTRIBUTES => []
             ],
             "get_finish_page" => [
                 Router::CONFIG_KEY_PATH => "/quiz/completed",
-                "controller" => "questionInstance",
-                "action" => "getReviewPage",
-                "method" => "GET",
-                "attributes" => []
+                Router::CONFIG_KEY_CONTROLLER => "questionInstance",
+                Router::CONFIG_KEY_ACTION => "getReviewPage",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => []
             ],
             "handle_error" => [
                 Router::CONFIG_KEY_PATH => "/error/{id}",
-                "controller" => "error",
-                "action" => "getErrorPage",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "error",
+                Router::CONFIG_KEY_ACTION => "getErrorPage",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "id" => '\d+'
                 ]
             ],
             "view_result" => [
                 Router::CONFIG_KEY_PATH => "/results/{offset}",
-                "controller" => "result",
-                "action" => "getAllUserTest",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "result",
+                Router::CONFIG_KEY_ACTION => "getAllUserTest",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     "offset" => '\d+'
                 ]
             ],
             "view_congrats" => [
                 Router::CONFIG_KEY_PATH => "/congrats",
-                "controller" => "questionInstance",
-                "action" => "getCongrats",
-                "method" => "GET",
-                "attributes" => []
+                Router::CONFIG_KEY_CONTROLLER => "questionInstance",
+                Router::CONFIG_KEY_ACTION => "getCongrats",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => []
             ],
             "view_score_page" => [
                 Router::CONFIG_KEY_PATH => "/result/user/{userId}/quiz/{quizId}",
-                "controller" => "result",
-                "action" => "getScorePage",
-                "method" => "GET",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "result",
+                Router::CONFIG_KEY_ACTION => "getScorePage",
+                Router::CONFIG_KEY_METHOD => "GET",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     'userId' => '\d+',
                     'quizId' => '\d+'
                 ]
             ],
             "score_quiz_instance" => [
                 Router::CONFIG_KEY_PATH => "/result/score/{quizId}",
-                "controller" => "result",
-                "action" => "scoreQuiz",
-                "method" => "POST",
-                "attributes" => [
+                Router::CONFIG_KEY_CONTROLLER => "result",
+                Router::CONFIG_KEY_ACTION => "scoreQuiz",
+                Router::CONFIG_KEY_METHOD => "POST",
+                Router::CONFIG_KEY_ATTRIBUTES => [
                     'quizId' => '\d+'
                 ]
             ]
