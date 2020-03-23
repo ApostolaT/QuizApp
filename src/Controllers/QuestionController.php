33848cc89953fd39dccb566c76eb30faab4b6c07
apostola <alexei.postolachi@evozon.com>
2020-03-23 17:28:37 +0200

    create question pagination

--- src/Controllers/QuestionController.php
+++ src/Controllers/QuestionController.php
@@ -1,44 +1,57 @@
 <?php
 
-
 // TODO if not routematch go to not found
-
 namespace QuizApp\Controllers;
-
 
 use Framework\Controller\AbstractController;
 use Framework\Http\Response;
 use Framework\Http\Stream;
 use Psr\Http\Message\RequestInterface;
 use QuizApp\Services\AbstractService;
-use ReallyOrm\Exceptions\NoSuchRowException;
+use QuizApp\Services\QuestionService;
+use QuizApp\Utils\PaginatorTrait;
 
 class QuestionController extends AbstractController
 {
+    use PaginatorTrait;
+    /**
+     * @var QuestionService
+     */
     private $questionService;
-
-    public function setService(AbstractService $questionService)
+    /**
+     * This function sets the questionService.
+     * Its injected from container
+     * @param AbstractService $service
+     */
+    public function setService(AbstractService $service)
     {
-        $this->questionService = $questionService;
+        $this->questionService = $service;
     }
-
+    /**
+     * This function renders all the questions from the repo in pages.
+     * The questions are taken from repo by calling the corresponding method from service,
+     * and paginated by using the paginator trait.
+     * @param RequestInterface $request
+     * @return Response
+     */
     public function listAll(RequestInterface $request)
     {
-        // TODO create a check function for session
-        if ($this->session->get('name') !== null) {
-            try {
-                $entities = $this->questionService->getAll($request);
-                return $this->renderer->renderView("admin-questions-listing.phtml", ['session' => $this->session, 'entities' => $entities]);
-            } catch (NoSuchRowException $e) {
-                return $this->renderer->renderView("admin-questions-listing.phtml", ['session' => $this->session]);
-            }
+        if ($this->session->get('role') !== 'admin') {
+            return $this->getRedirectPage("http://local.quiz.com/error/404");
         }
-        $response = new Response(Stream::createFromString(' '), []);
-        $response = $response->withStatus(301);
-        $response = $response->withHeader('Location', 'http://local.quiz.com');
+        $paginator = $this->createPaginationForRequestWithService($request, $this->questionService);
+        $renderParams = [
+            'session' => $this->session,
+            //message is for the future messages that will be displayed
+            //on admin-results-listing.phtml page
+            'message' => $this->session->get('message'),
+            'paginator' => $paginator,
+            'entities' =>  $this->questionService->getAll($paginator->getCurrentPage())
+        ];
 
-        return $response;
+        return $this->renderer->renderView('admin-questions-listing.phtml', $renderParams);
     }
+
 
     public function goToAddQuestion(RequestInterface $request)
     {
@@ -55,10 +68,10 @@ class QuestionController extends AbstractController
     public function addQuestion(RequestInterface $request)
     {
         if ($this->session->get('name') !== null) {
-            if ($this->questionService->createEntity($request, $this->session)) {
+            if ($this->questionService->createEntity($request)) {
                 $response = new Response(Stream::createFromString(' '), []);
                 $response = $response->withStatus(301);
-                $response = $response->withHeader('Location', 'http://local.quiz.com/question/1');
+                $response = $response->withHeader('Location', 'http://local.quiz.com/question');
 
                 return $response;
             }
@@ -77,7 +90,7 @@ class QuestionController extends AbstractController
             if ($this->questionService->delete($request)) {
                 $response = new Response(Stream::createFromString(' '), []);
                 $response = $response->withStatus(301);
-                $response = $response->withHeader('Location', 'http://local.quiz.com/question/1');
+                $response = $response->withHeader('Location', 'http://local.quiz.com/question');
 
                 return $response;
             }
@@ -111,7 +124,7 @@ class QuestionController extends AbstractController
             if ($this->questionService->updateEntity($request, $this->session)) {
                 $response = new Response(Stream::createFromString(' '), []);
                 $response = $response->withStatus(301);
-                $response = $response->withHeader('Location', 'http://local.quiz.com/question/1');
+                $response = $response->withHeader('Location', 'http://local.quiz.com/question');
 
                 return $response;
             }
